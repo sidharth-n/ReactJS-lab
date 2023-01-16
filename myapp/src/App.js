7b8aa685a15be5cb8cdf26556cfc05e89500cc02
sidharth-n <sidharthnofficial@gmail.com>
2023-01-16 13:47:23 +0530

    adding components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const Person = () => {
+  return (
+    <>
+      <h1>Name:sid</h1>
+      <h2>Last name : crizz</h2>
+      <h2>Age : 30</h2>
+    </>
+  );
+};
+
 function App() {
+  const name = "sid";
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <>
+        <h1>Hello, {Math.pow(4, 8)}</h1>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+      </>
     </div>
   );
 }
