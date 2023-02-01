d1ee5f2426e5bae72800b58bcb795ea016d2df19
sidharth-n <sidharthnofficial@gmail.com>
2023-02-01 12:55:29 +0530

    hacker-news-clone

---
+++ HN-clone/hacker-news-clone/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Story from "./story";
+import "./style.css";
+
+const HackerNews = () => {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then((response) => response.json())
+      .then(async (data) => {
+        const storyPromises = data
+          .slice(0, 100)
+          .map((storyId) =>
+            fetch(
+              `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+            ).then((res) => res.json())
+          );
+        const storyData = await Promise.all(storyPromises);
+        console.log(storyData);
+        setStories(storyData);
+      });
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="top">
+        <div className="top-head">
+          Hacker News
+          <div className="top-sub">
+            new | past | comments | ask | show | jobs | submit
+          </div>
+        </div>
+        <div className="login">login</div>
+      </div>
+      <ol className="news">
+        {stories.map((story) => (
+          <li>
+            <Story props={story} />
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default HackerNews;
---
+++ HN-clone/hacker-news-clone/src/story.js
@@ -0,0 +1,38 @@
+export default function Story({ props }) {
+  function hoursPassed(timestamp) {
+    const now = Date.now() / 1000; // current time in Unix timestamp (in seconds)
+    const difference = now - timestamp; // difference in seconds
+    const hours = difference / 3600; // convert to hours
+    return Math.floor(hours);
+  }
+  const UpdateTime = hoursPassed(props.time);
+  return (
+    <li className="story" key={props.id}>
+      <div className="story-top">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="7"
+          height="7"
+          viewBox="0 0 24 24"
+          fill="grey"
+          stroke="grey"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
+        </svg>
+        <div className="story-title">{props.title}</div>
+        <div className="story-url" href={props.url}>
+          {props.url
+            ? `(${props.url.replace("https://", "").replace("www.", "")})`
+            : ""}
+        </div>
+      </div>
+      <div className="story-sub">
+        {props.score} points by {props.by} {UpdateTime} hours ago | hide |{" "}
+        {props.descendants} comments
+      </div>
+    </li>
+  );
+}
