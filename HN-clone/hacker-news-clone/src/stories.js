e36dd21e2074c0eb9ab8aeb2b6e5aae9242f9846
sidharth-n <sidharthnofficial@gmail.com>
2023-02-07 11:01:27 +0530

    temperoy project backup

--- HN-clone/hacker-news-clone/src/stories.js
+++ HN-clone/hacker-news-clone/src/stories.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Comments from "./comments";
+import { Link } from "react-router-dom";
 import Loading from "./loading";
 import Story from "./story";
 
@@ -7,22 +7,29 @@ const Stories = () => {
   const [stories, setStories] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
-      .then((response) => response.json())
-      .then(async (data) => {
-        const storyPromises = data
-          .slice(0, 100)
-          .map((storyId) =>
-            fetch(
-              `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
-            ).then((res) => res.json())
-          );
-        const storyData = await Promise.all(storyPromises);
+    const cachedData = localStorage.getItem("stories");
+    if (cachedData) {
+      setStories(JSON.parse(cachedData));
+      setLoading(false);
+    } else {
+      fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+        .then((response) => response.json())
+        .then(async (data) => {
+          const storyPromises = data
+            .slice(0, 100)
+            .map((storyId) =>
+              fetch(
+                `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+              ).then((res) => res.json())
+            );
+          const storyData = await Promise.all(storyPromises);
 
-        console.log(storyData);
-        setStories(storyData);
-        setLoading(false);
-      });
+          console.log(storyData);
+          setStories(storyData);
+          setLoading(false);
+          localStorage.setItem("stories", JSON.stringify(storyData));
+        });
+    }
   }, []);
 
   return (
@@ -31,7 +38,9 @@ const Stories = () => {
       <ol className="news">
         {stories.map((story) => (
           <li key={story.id}>
-            <Story props={story} />
+            <Link className="link" to={`/comments/${story.id}`}>
+              <Story props={story} />
+            </Link>
           </li>
         ))}
       </ol>
---
+++ temp.js
@@ -0,0 +1,82 @@
+const puppeteer = require("puppeteer");
+
+async function selectFirstMessage(page) {
+  await page.goto("https://web.whatsapp.com/");
+
+  // Wait for the page to load
+  await page.waitForXPath('//*[@id="app"]/div/div/div[3]');
+
+  const BotGroup = await page.waitForSelector(
+    `.ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr._11JPr[title="AI test"]`
+  );
+
+  BotGroup.click();
+  const lastMessage = await page.waitForXPath(
+    "/html/body/div[1]/div/div/div[4]/div/div[2]/div/div[2]/div[2]/div[6]/div/div/div[1]/div[1]/div[1]/div/span[1]/span"
+  );
+  currentText = await page.evaluate(
+    (element) => element.textContent,
+    lastMessage
+  );
+  console.log(currentText);
+  // Select the first chat
+  /*   const firstChat = await page.evaluate(() => {
+    const chat = document.querySelector("#pane-side [tabindex='0']");
+    chat.click();
+    return chat;
+  }); */
+
+  // Wait for the chat to open
+  ///html/body/div[1]/div/div/div[4]/div/div[2]/div/div[2]/div[2]/div[6]/div/div/div[1]/div[1]/div[1]/div/span[1]/span
+}
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // Select the first message
+  await selectFirstMessage(page);
+})();
+async function fetchAnswer(question) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://you.com/search?q=who+are+you&tbm=youchat&cfr=chat");
+
+  // Input field selection
+  const inputField = await page.waitForXPath(
+    "/html/body/div[1]/div/div/div/div[2]/section/main/div/div/div[1]/div[2]/div/div/li/div[2]/div[1]/textarea"
+  );
+  await inputField.type(question);
+  await inputField.press("Enter");
+
+  // Get text content
+  const text = await page.waitForXPath(
+    '//*[@id="chatHistory"]/div[2]/div[2]/div[1]/p'
+  );
+
+  let previousText = "";
+  let currentText = "";
+  let sameTextCount = 0;
+
+  while (sameTextCount < 5) {
+    previousText = currentText;
+    currentText = await page.evaluate((element) => element.textContent, text);
+    if (previousText === currentText) {
+      sameTextCount++;
+    } else {
+      sameTextCount = 0;
+    }
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  }
+  await browser.close();
+  return currentText;
+}
+
+/* (async () => {
+  const answer1 = await fetchAnswer("write a poem about chatgpt");
+  console.log("Answer 1:", answer1);
+
+  const answer2 = await fetchAnswer("who is the CEO of Twitter");
+  console.log("Answer 2:", answer2);
+})();
+ */
