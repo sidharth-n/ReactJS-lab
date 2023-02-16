0c74e8d6ab71d13fc3c1144ee7774b967b87f70b
sidharth-n <sidharthnofficial@gmail.com>
2023-02-16 14:13:57 +0530

    start to share screen done

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,13 +1,20 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const url = "https://textdb.dev/api/data/tic-tac-toe-test-00456";
+  let url;
   const [board, setBoard] = useState(Array(16).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
   const [winner, setWinner] = useState(null);
   const [gameEnded, setGameEnded] = useState(false);
   const [winnerLine, setWinnerLine] = useState(Array(4).fill(null));
   const [count, setCount] = useState(0);
+  const [isRoomCreated, setisRoomCreated] = useState(false);
+  const [isPlayerChoosen, setisPlayerChoosen] = useState(false);
+  const [roomLink, setRoomLink] = useState("Link here");
+  const [playerChoosen, setPlayerChoosen] = useState(null);
+  const [isShared, setIsShared] = useState(false);
+  let shareLink;
+  let id;
   const arraysEqual = (a, b) => {
     if (a === b) return true;
     if (a == null || b == null) return false;
@@ -17,6 +24,50 @@ function App() {
     }
     return true;
   };
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    if (queryString != "") {
+      const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
+      url = `https://textdb.dev/api/data/${params.id}`;
+      setIsShared(true);
+      setisPlayerChoosen(true);
+      setisRoomCreated(true);
+      setPlayerChoosen(params.pl);
+      setisPlayerChoosen(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    setRoomLink(`${window.location.href}?id=${id}&pl=${playerChoosen}`);
+  }, [playerChoosen]);
+
+  const getId = function (_id = null) {
+    const getRandomId = () => {
+      const randomChar = () =>
+        String.fromCharCode(97 + Math.floor(26 * Math.random()));
+      return [1, 1, 1]
+        .map((_) => {
+          return [1, 1, 1].map(randomChar).join("");
+        })
+        .join("-");
+    };
+
+    if (!_id) {
+      _id = getRandomId();
+    }
+
+    return _id;
+  };
+
+  function createRoom() {
+    id = getId();
+    url = `https://textdb.dev/api/data/${id}`;
+    console.log(url);
+    setisRoomCreated(true);
+  }
 
   useEffect(() => {
     const boardArray = JSON.stringify(board);
@@ -29,7 +80,11 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
+  function setGame() {
+    window.location = roomLink;
+  }
+
+  /*   useEffect(() => {
     const intervalId = setInterval(() => {
       setCount((prevCount) => prevCount + 1);
     }, 1000);
@@ -49,7 +104,7 @@ function App() {
         }
       });
     return () => clearInterval(intervalId);
-  }, [count]);
+  }, [count]); */
 
   useEffect(() => {
     const result = calculateWinner(board);
@@ -99,22 +154,103 @@ function App() {
   }
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="inline-flex items-center justify-center rounded-full bg-gray-200 px-2.5 py-0.5 text-gray-700 mt-4 mb-4">
-        {winner
-          ? winner + " won"
-          : gameEnded
-          ? "Oops!! replay ?"
-          : "Next Move : " + currentPlayer}
-      </div>
-      <Board handleClick={handleClick} board={board} winnerLine={winnerLine} />
-      <button
-        className={`bg-gray-500 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded-lg mt-4 ${
-          gameEnded || winner ? "block" : "hidden"
+      <div
+        class={`mx-4 mt-44 flex max-w-sm flex-col items-center rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800 ${
+          isRoomCreated ? "hidden" : "flex"
         }`}
-        onClick={clearBoard}
       >
-        Replay
-      </button>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          React tik-tak-toe
+        </h5>
+        <p class="mb-6 text-center font-normal text-gray-700 dark:text-gray-400">
+          Now play tik-tac-toe with your friend online. Enjoy your old classroom
+          vibes.
+        </p>
+        <div
+          onClick={createRoom}
+          class="inline-flex items-center self-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Create New Game
+        </div>
+      </div>
+      <div
+        class={`mx-4 mt-44 max-w-sm flex-col items-center rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800 ${
+          !isPlayerChoosen && isRoomCreated ? "flex" : "hidden"
+        }`}
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Choose your player
+        </h5>
+        <div className="flex gap-4">
+          <div
+            onClick={() => {
+              setPlayerChoosen("X");
+              setisPlayerChoosen(true);
+            }}
+            class="inline-flex items-center self-center rounded-full bg-gray-300 px-5 py-2 text-center text-3xl font-medium text-white hover:bg-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-300 "
+          >
+            X
+          </div>
+          <div
+            onClick={() => {
+              setPlayerChoosen("O");
+              setisPlayerChoosen(true);
+            }}
+            class="inline-flex items-center self-center rounded-full bg-gray-300 px-5 py-2 text-center text-3xl font-medium text-white hover:bg-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-300 "
+          >
+            O
+          </div>
+        </div>
+      </div>
+      <div
+        class={`mx-4 mt-44 max-w-sm flex-col items-center rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800 ${
+          isShared || !isPlayerChoosen ? "hidden" : "flex"
+        } `}
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Share the link
+        </h5>
+        <div className="flex flex-col gap-4">
+          <div class="inline-flex items-center self-center text-center text-sm font-medium text-blue-700 hover:bg-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-300 ">
+            {roomLink}
+          </div>
+          <div
+            onClick={() => {
+              setIsShared(true);
+              setGame();
+            }}
+            class="inline-flex items-center self-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Done
+          </div>
+        </div>
+      </div>
+      <div
+        className={`flex flex-col justify-center items-center ${
+          isRoomCreated && isShared ? "flex" : "hidden"
+        }`}
+      >
+        <div className="inline-flex items-center justify-center rounded-full bg-gray-200 px-2.5 py-0.5 text-gray-700 mt-4 mb-4">
+          {winner
+            ? winner + " won"
+            : gameEnded
+            ? "Oops!! replay ?"
+            : "Next Move : " + currentPlayer}
+        </div>
+        <Board
+          handleClick={handleClick}
+          board={board}
+          winnerLine={winnerLine}
+        />
+        <button
+          className={`bg-gray-500 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded-lg mt-4 ${
+            gameEnded || winner ? "block" : "hidden"
+          }`}
+          onClick={clearBoard}
+        >
+          Replay
+        </button>
+      </div>
     </div>
   );
 }
